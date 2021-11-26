92cd20e73e95166c111e6fd94c7460df591308d5
Tunkiz <18015037@mvula.univen.ac.za>
2021-11-26 11:32:25 +0200

    commit with this

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { LeftSection } from './HomeStyles';
+import { FaSmileWink } from 'react-icons/fa';
 
 const Hero = (props) => (
   <>
     <Section row nopadding id="home">
       <LeftSection>
         <SectionTitle main center>
-          Hi, i'm Tunkis😎. Welcome. <br />
+          Hi, i'm Tunkis<FaSmileWink size="4rem" />. Welcome. <br />
           
         </SectionTitle>
         <SectionText>
@@ -15,10 +16,9 @@ const Hero = (props) => (
         I am a Confident, Honourable and Loving person. Software development is my passion and I love exploring new technologies.
         </SectionText>
         <SectionText>
-            I want to be a developer at <b>SovTech</b> because I want to learn from the best #WORLDCLASS. I read the company’s mission and vision and after finding out
-            more information about the company from social media, I can clearly see how my goals align with the purposes of the
-            company. There's nothing I love more than when I am working with other engaged individuals towards a common goal,
-            and that's what excited me about this opportunity. <b>i have the artistry, engineering and I can work with people.</b>
+            I want to be a developer at <b>SovTech</b> because I want to learn from the best #WORLDCLASS. I read the company’s mission and vision. After conducting my research I realized how my goals align with the purposes of the
+            company. I have nothing but respect for the technological innovations SovTech has been developing. And I want nothing more than to be a part of a company that is changing the world. I see that as a way of moving forward in 
+            my career. <b>I have the artistry, engineering and I can work with people.</b>
             This opportunity will enable me to showcase my skills and be a valuable team member who is able to grow and lead others.
         </SectionText>
       </LeftSection>
